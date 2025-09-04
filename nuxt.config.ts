f8b9ed21eba47d4cf1d6b5187dfeb20ace3eba25
Bolizz <bolsynj@gmail.com>
2025-09-04 05:13:00 +0500

    name changed final

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
-      title: "Bolsyn.dev",
+      title: "bolsyn.dev",
       meta: [
         {
           name: "Personal Portfolio",
